a689760bae4db6341d33c1fdce9e47e72cd8bd1c
Lucas Cruz <lucasmarquioridacruz@gmail.com>
2026-03-10 17:07:07 -0300

    aqui eu criei todos arquivos necessarios para rodas a pasta lista.clientes

--- vendas/src/app.module.ts
+++ vendas/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthCheckModule } from './health-check/health-check.module';
+import { ListaClientesModule } from './lista.clientes/lista.clientes.module';
 
 @Module({
-  imports: [HealthCheckModule],
+  imports: [HealthCheckModule,ListaClientesModule],
   controllers: [],
   providers: [],
 })
